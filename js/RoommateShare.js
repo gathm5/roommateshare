bbc8e0732ec379bfcb7b65db2a009c19022fa517
gauthamstalin <gauthamstalin@gmail.com>
2013-10-20 19:48:23 -0700

    Roommate Share Github

---
+++ js/RoommateShare.js
@@ -0,0 +1,19 @@
+var RoommateShare = ((function(){
+	var module = {};
+	module.Init = function(){
+		console.log('Initialized');
+	};
+	module.FindRental = function(){
+		
+	};
+	module.Login = function(){
+		
+	};
+	module.Register = function(){
+		
+	};
+	module.PostRental = function(){
+		
+	};
+	return module;
+})());
