58c0c2d06f0ff18b2aca732d82f1a4292569e081
gauthamstalin <gauthamstalin@gmail.com>
2013-10-20 21:09:31 -0700

    .

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <!doctype html>
 <html>
     <head>
-        <title></title>
+        <title>Roommate Share | finding the right place</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalabale=false">
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="fragment" content="!" />
+        <link href='https://fonts.googleapis.com/css?family=Expletus+Sans' rel='stylesheet' type='text/css'>
+        <link href='css/siteprop.css' rel='stylesheet' type='text/css'>
+        <link href='css/roommateshare.css' rel='stylesheet' type='text/css'>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-        <script type="text/javascript"></script>
     </head>
     <body>
 
+        <script type="text/javascript" src="js/RoommateShare.js"></script>
+        <script type="text/javascript">
+            RoommateShare.Init();
+        </script>
     </body>
 </html>
--- js/RoommateShare.js
+++ js/RoommateShare.js
@@ -1,19 +1,31 @@
-var RoommateShare = ((function(){
-	var module = {};
-	module.Init = function(){
-		console.log('Initialized');
-	};
-	module.FindRental = function(){
-		
-	};
-	module.Login = function(){
-		
-	};
-	module.Register = function(){
-		
-	};
-	module.PostRental = function(){
-		
-	};
-	return module;
-})());
+(function() {
+    var o = $({});
+    $.subscribe = function() {
+        o.on.apply(o, arguments);
+    };
+    $.unsubscribe = function() {
+        o.off.apply(o, arguments);
+    };
+    $.publish = function() {
+        o.trigger.apply(o, arguments);
+    };
+}());
+var RoommateShare = ((function($) {
+    var module = {}, body = $('body');
+    module.Init = function(){
+        body.addClass('loaded');
+    };
+    module.FindRental = function() {
+	
+    };
+    module.Login = function() {
+	
+    };
+    module.Register = function() {
+	
+    };
+    module.PostRental = function() {
+	
+    };
+    return module;
+})(jQuery));
